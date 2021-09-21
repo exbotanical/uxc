fb9560d1f151ac762d09e54d638343d80486b1cd
goldmund <matthewtzito@gmail.com>
2021-09-21 09:33:04 -0700

    chore: scaffold login state machine

---
+++ src/components/auth/Login.tsx
@@ -0,0 +1,13 @@
+import { Component, createRef } from 'react';
+
+class Login extends Component {
+	emailInputRef = createRef();
+	passwordRef = createRef();
+	submitBtnRef = createRef();
+
+	render () {
+		return null;
+	}
+}
+
+export default Login;
